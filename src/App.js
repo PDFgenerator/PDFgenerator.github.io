45155ba096ce7cc78c8ec5113d9055cbf170cfed
Exodya21 <jaumegomiciai@gmail.com>
2022-04-19 13:30:45 +0200

    prubeal

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Pvto Che
+          Learn React Pvto Che aaa
         </a>
       </header>
     </div>
