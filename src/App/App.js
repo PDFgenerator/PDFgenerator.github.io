288a16df93e62969bb108df83b37e199219924f9
lucas martinez <patrasioo69@gmail.com>
2022-04-27 11:08:39 +0200

    add footer ans styles

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,14 @@
 import css from './App.module.css';
 import Nav from './Nav/nav.jsx';
 import Table from './Inovoice/table.jsx';
+import { Footer } from './Inovoice/Footer/footer';
 
 function App() {
   return (
     <div className={css.ContainerApp}>
       <Nav />
       <Table />
+      <Footer />
     </div>
   );
 }
---
+++ src/App/Inovoice/Footer/footer.jsx
@@ -0,0 +1,29 @@
+import css from './footer.module.css';
+import { DataService } from '../../../Repository/DataService';
+import { useState } from 'react';
+import email from '../../../Assets/mail.svg';
+import telefono from '../../../Assets/telefono.svg';
+
+export function Footer() {
+
+    const [info, setInfo]= useState(DataService.footer)
+
+    return(
+        <div className={css.Footer}>
+            <section className={css.ContaineLines}>
+                <div className={css.Line}></div>
+                <h2 className={css.Title}>Fasana Export-Import S.L.</h2>
+                <div className={css.Line}></div>
+            </section>
+            <article>
+                <p>{info.code}</p>
+                <p>{info.direction}</p>
+                <div className={css.Contacto}>
+                    <p><img src={telefono} alt="telefono" />{info.numberPhone}</p>
+                    <p><img src={email} alt="email" />{info.email}</p>
+                </div>
+            </article>
+        </div>
+    )
+
+}
