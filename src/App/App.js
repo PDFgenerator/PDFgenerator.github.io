5609f9218f1509c080101776ea8df6e977a24706
lucas martinez <patrasioo69@gmail.com>
2022-05-02 11:18:11 +0200

    link component Header in app

--- src/App/App.js
+++ src/App/App.js
@@ -2,11 +2,13 @@ import css from './App.module.css';
 import Nav from './Nav/nav.jsx';
 import Table from './Inovoice/table.jsx';
 import Footer from './Inovoice/Footer/footer';
+import Header from './Inovoice/Header/header';
 
 function App() {
   return (
     <div className={css.ContainerApp}>
       <Nav />
+      <Header />
       <Table />
       <Footer />
     </div>
