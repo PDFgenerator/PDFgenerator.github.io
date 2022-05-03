9621c2737d7819ff0d8812e2eefbc57773c1b645
Exodya21 <jaumegomiciai@gmail.com>
2022-05-03 14:16:54 +0200

    title date time and logo added in PDF function

--- src/App/Nav/nav.jsx
+++ src/App/Nav/nav.jsx
@@ -1,16 +1,13 @@
 import css from './nav.module.css';
 import burger from '../../Assets/burger.svg';
-import { PDF } from '../../Repository/PDF';
+import PDF from '../../Repository/PDF.js';
 
 function Nav() {
-
-    PDF()
-
     return (
         <nav className={css.nav}>
             <p>Digital Invoice</p>
             <div className={css.Generate}>
-                <button /*onClick={ () => pdf() }*/><p>Generate PDF</p></button>
+                <button onClick={ () => PDF() }><p>Generate PDF</p></button>
             </div>
             <div className={css.ContainerBurguer}><img src={burger} alt="" /></div>
         </nav>
--- src/Repository/PDF.js
+++ src/Repository/PDF.js
@@ -1,8 +1,42 @@
 import { jsPDF } from "jspdf";
+import { DateTime } from "./Date";
+import logo from "../Assets/logo.png";
 
-export function PDF() {
+function PDF() {
     const doc = new jsPDF();
+    // console.log(doc.getFontList())
 
-        doc.text("Hello Word!", 10 , 10);
-        doc.save("prueba1.pdf");
+    /* Title */
+    doc.setFontSize(13);
+    doc.setFont("Helvetica", "Bold");
+    doc.text("Fasana Export - Import S.L.", 6 , 12);
+    doc.line(6, 13, 105, 13);
+
+    /* Date */
+    doc.setFontSize(10);
+    doc.setFont("Helvetica", "Bold");
+    doc.text("DATE", 141 , 12);
+    doc.line(141, 13, 151, 13);
+
+    doc.setFontSize(10.5);
+    doc.setFont("Helvetica", "");
+    doc.text(DateTime().date, 141 , 17.5);
+
+    /* Time */
+    doc.setFontSize(10);
+    doc.setFont("Helvetica", "Bold");
+    doc.text("TIME", 171 , 12);
+    doc.line(171, 13, 181, 13);
+
+    doc.setFontSize(10.5);
+    doc.setFont("Helvetica", "");
+    doc.text(DateTime().time, 171 , 17.5);
+
+    /* Logo */
+    doc.addImage(logo, "PNG", 10, 18, 54, 15, "logo", "NONE", 0);
+
+
+    doc.save("prueba1.pdf");
 }
+
+export default PDF;
